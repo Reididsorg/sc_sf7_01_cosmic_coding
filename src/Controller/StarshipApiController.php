c96b13e0e197924ab48f19160e74c7409d74ba88
bruno.grosdidier@2dcom.fr <15422030+Reididsorg@users.noreply.github.com>
2026-03-18 12:05:00 +0100

    Utilisation du service LoggerInterface dans le contrôleur par injection de dépendance

--- src/Controller/StarshipApiController.php
+++ src/Controller/StarshipApiController.php
@@ -2,6 +2,7 @@
 namespace App\Controller;
 
 use App\Model\Starship;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,8 +10,12 @@ use Symfony\Component\Routing\Attribute\Route;
 class StarshipApiController extends AbstractController
 {
     #[Route('/api/starships')]
-    public function getCollection(): Response
+    public function getCollection(LoggerInterface $logger): Response
     {
+        dump($logger);
+
+        $logger->info('Starship collection retrieved');
+
         $starships = [
             new Starship(
                 1,

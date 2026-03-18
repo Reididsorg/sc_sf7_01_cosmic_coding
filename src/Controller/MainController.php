3eb73945adddd85f3313f1a6b71596afc278f8d5
bruno.grosdidier@2dcom.fr <15422030+Reididsorg@users.noreply.github.com>
2026-03-18 13:33:12 +0100

    Création d'un service perso : le repository StarshipRepository, qui fournit un tableau d'objets Starship, utilisé pour retourner un objet JSON dans le StarshipApiController et retourner un twig dans le MainController

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -1,20 +1,26 @@
 <?php
+
 namespace App\Controller;
+
+use App\Repository\StarshipRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+
 class MainController extends AbstractController
 {
+    public function __construct(
+        private readonly StarshipRepository $starshipRepository,
+    ) {
+    }
+
     #[Route('/')]
     public function homepage(): Response
     {
-        $starshipCount = 457;
-        $myShip = [
-            'name' => 'USS LeafyCruiser (NCC-0001)',
-            'class' => 'Garden',
-            'captain' => 'Jean-Luc Pickles',
-            'status' => 'under construction',
-        ];
+        $ships = $this->starshipRepository->findAll();
+        $starshipCount = count($ships);
+        $myShip = $ships[array_rand($ships)];
+
         return $this->render('main/homepage.html.twig', [
             'numberOfStarships' => $starshipCount,
             'myShip' => $myShip,
---
+++ src/Repository/StarshipRepository.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Repository;
+
+use App\Model\Starship;
+use Psr\Log\LoggerInterface;
+
+readonly class StarshipRepository
+{
+    public function __construct(
+        private LoggerInterface $logger,
+    ) {
+    }
+
+    public function findAll(): array
+    {
+        $this->logger->info('Starship collection retrieved from StarshipRepository');
+
+        return [
+            new Starship(
+                1,
+                'USS LeafyCruiser (NCC-0001)',
+                'Garden',
+                'Jean-Luc Pickles',
+                'taken over by Q'
+            ),
+            new Starship(
+                2,
+                'USS Espresso (NCC-1234-C)',
+                'Latte',
+                'James T. Quick!',
+                'repaired',
+            ),
+            new Starship(
+                3,
+                'USS Wanderlust (NCC-2024-W)',
+                'Delta Tourist',
+                'Kathryn Journeyway',
+                'under construction',
+            ),
+        ];
+    }
+}

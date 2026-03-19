2c0ca3b13c47d3fa6819234a78e9ed64c0b250be
bruno.grosdidier@2dcom.fr <15422030+Reididsorg@users.noreply.github.com>
2026-03-19 14:24:47 +0100

    Amélioration de l'nimation et des commentaires

--- assets/controllers/closeable_controller.js
+++ assets/controllers/closeable_controller.js
@@ -1,15 +1,9 @@
 import { Controller } from '@hotwired/stimulus';
 
-/*
- * This is an example Stimulus controller!
- *
- * Any element with a data-controller="hello" attribute will cause
- * this controller to be executed. The name "hello" comes from the filename:
- * hello_controller.js -> "hello"
- *
- * Delete this file or adapt it for your use!
- */
 export default class extends Controller {
+
+    // Quand l'animation est terminée, l'élément est supprimé.
+    // async puisqu'il y a await à l'intérieur
     async close() {
         this.element.style.width = '0';
 
